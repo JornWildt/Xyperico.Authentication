dd254e1cb5425e9ee35a225bb71b1362d7523ad1
JornWildt <jw@fjeldgruppen.dk>
2012-12-07 07:41:14 +0100

    Improve script generation.

---
+++ Xyperico.Authentication.Web/Html/UserNameValidatorHtmlHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using Xyperico.Web.Mvc;
+
+
+namespace Xyperico.Authentication.Web.Html
+{
+  public static class UserNameValidatorHtmlHelper
+  {
+    public static IHtmlString UserNameValidatorFor<TModel, TProperty>(
+      this HtmlHelper<TModel> html,
+      Expression<Func<TModel, TProperty>> expression)
+    {
+      ModelMetadata meta = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
+      
+      string inputId = meta.PropertyName;
+      string spinnerImageUrl = html.ImageUrl("load-indicator-16x16.gif").ToString();
+      
+      string script = string.Format(@"<span id=""{0}Status""></span>
+<script type=""text/javascript"">
+  $(document).ready(function () {{
+    UserNameChecker.Initialize(""#{0}"", ""#{0}Status"", ""{1}"");
+  }});
+</script>",
+          inputId,
+          spinnerImageUrl);
+
+      return new MvcHtmlString(script);
+    }
+  }
+}
